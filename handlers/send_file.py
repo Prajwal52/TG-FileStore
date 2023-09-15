98bf6b83a15d06bcc4d72b805619abab9edaa595
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-09-15 12:25:46 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -9,7 +9,7 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**Files will be Deleted After 30 min ⏰**\n",
+            f"**Files will be Deleted After 10 min ⏰**\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -30,7 +30,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 
 async def delete_file(file_id: int):
-    await asyncio.sleep(1800)  # wait for 30 minutes
+    await asyncio.sleep(600)  # wait for 10 minutes
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
