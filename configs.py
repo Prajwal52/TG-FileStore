92c5b10446e0c579f4844871d510b1ac863a5e74
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-09-15 14:32:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,6 +17,7 @@ class Config(object):
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+	PORT = os.environ.get("PORT", "8080")
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
