56903fee77695349b24eb877291689ffe8410b20
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-09-15 12:48:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "SaiFilestoreBot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001949670058"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1966376217"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://filestore:filestore@cluster0.izqfu2d.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001687768591")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
